1211dc6cffc3862da10400314affe85daba3ee24
Sean Lally <seanlally8@gmail.com>
2026-04-22 17:32:19 -0400

    wrote tokenizer .h file

---
+++ include/JackTokenizer.h
@@ -0,0 +1,19 @@
+
+// JackTonizer.h
+#ifndef JACKTOKENIZER_H // check if myheader_h is not defined
+#define JACKTOKENIZER_H
+
+#include <stdio.h>
+
+// all your header file code (declarations, etc.) goes here
+int hasMoreTokens(FILE *file);
+void advance(char *token, FILE *file);
+char *tokenType(char *token);
+char *keyword(char *token);
+char symbol(char *token);
+char *identifier(char *token);
+int intVal(char *token);
+char *stringVal(char *token);
+
+
+#endif // ends the #ifndef block
---
+++ src/JackTokenizer.c
@@ -0,0 +1,40 @@
+#include "JackTokenizer.h"
+
+int hasMoreTokens(FILE *file) {
+
+  return 0;
+}
+
+void advance(char *token, FILE *file) {
+
+}
+
+char *tokenType(char *token) {
+
+  return "temp";
+}
+
+char *keyword(char *token) {
+
+  return "temp";
+}
+
+char symbol(char *token) {
+
+  return 't';
+}
+
+char *identifier(char *token) {
+
+  return "temp";
+}
+
+int intVal(char *token) {
+
+  return 0;
+}
+
+char *stringVal(char *token) {
+
+  return "temp";
+}
